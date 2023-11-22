0f10f53ac0a3a17fb5e15cebecd1e323edc6635a
SabrinaJennifer <56978550+SabrinaJennifer@users.noreply.github.com>
2023-11-22 20:54:31 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+
+
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Teste commit</Text>
-      <StatusBar style="auto" />
+  const [cep, setCep] = useState("")
+  const [logradouro, setLogradouro] = useState("")
+  const [bairro, setBairro] = useState("")
+  const [cidade, setCidade] = useState("")
+  const [uf, setUF] = useState("")
+  
+  return(
+    <View style={StyleSheet.container}>
+  <view style={styles.topbar}>
+      <Text style={styles.titulo}>
+        Buscar Cep
+      </Text>
+      </view>
+      <View style={styles.containerCep}>
+      <TextInput
+      style={{borderColor:"000000", borderWidth:2,}}
+      width:150, fontSize:18, marginTop: 40, MarginEnd: 20, borderRadius: 10
+      />
+
+      </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
+  containerCep:{
+    flexDirection: "row",
+    height: 100,
+    marginHorizontal: 20
+
+  },
+})
+
+const styles = StyleSheet({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'column'
   },
-});
+  topbar:{
+    flexDirection: "row",
+    height:70,
+    backgroundColor: '#219ebc',
+  },
+  titulo:{
+    color: '#FFFFFF',
+    fontSize: 25,
+    fontWeight: "bold",
+    alignSelf:"center",
+    margin:20
+  }
+})
